cd826516a8c051014a815f8ac58c7d146423669a
Chris Ritter <chrisritter2@gmail.com>
2021-04-20 18:57:51 -0400

    added implementation

--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -2,21 +2,35 @@
 #include "stdlib.h"
 
 #include "../includes/waiter.h"
+#include "../includes/externs.h"
 
 using namespace std;
 
-Waiter::Waiter(int id,std::string filename):id(id),myIO(filename){
-}
+Waiter::Waiter(int id,std::string filename):id(id),myIO(filename){}
 
-Waiter::~Waiter()
-{
-}
+Waiter::~Waiter(){}
 
 //gets next Order(s) from file_IO
 int Waiter::getNext(ORDER &anOrder){
-	return SUCCESS;
+	return myIO.getNext(anOrder);
 }
 
+//contains a loop that will get orders from filename one at a time
+//then puts them in order_in_Q then signals baker(s) using cv_order_inQ
+//so they can be consumed by baker(s)
+//when finished exits loop and signals baker(s) using cv_order_inQ that
+//it is done using b_WaiterIsFinished
 void Waiter::beWaiter() {
+	bool run = true;
+	int orderStatus;
+
+	while (run){
+		ORDER currentOrder;
+		orderStatus=this->getNext(currentOrder);
+
+		if (orderStatus==SUCCESS){
+			b_WaiterIsFinished=true;
+		}
+	}
 }
 
